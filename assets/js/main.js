e509ddab463b6dded53a9664f00329ef3b06b584
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-11-18 14:28:47 +0100

    richiesta soddisfatta

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,37 @@
 // Se è <= 5 il quadrato diventa giallo,
 // se è > di 5 il quadrato diventa verde.
 // Il numero ottenuto appare al centro del quadrato
+
+$(document).ready(function(){
+
+
+$(".row div").click(function(){
+  var questoDiv = $(this);
+  var questoP = $(this).children("p");
+  $.ajax({
+    url : "https://flynn.boolean.careers/exercises/api/random/int",
+    method : "GET",
+    success : function (numeroRitorno) {
+      var num = numeroRitorno.response;
+      if (num <= 5) {
+        $(questoP).html(num);
+        $(questoDiv).css("background", "yellow");
+      } else if (num > 5) {
+        $(questoP).html(num);
+        $(questoDiv).css("background", "green");
+      }
+    },
+    error : function (richiesta,stato,errori) {
+      alert("E' avvenuto un errore. "+ errore);
+    }
+  });
+
+});
+
+
+
+
+
+
+
+});
