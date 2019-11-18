603aada85372115227819baabdc85ffffa46691a
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-11-18 16:22:50 +0100

    prove per rendere inattivo click

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,36 +3,47 @@
 // se è > di 5 il quadrato diventa verde.
 // Il numero ottenuto appare al centro del quadrato
 
-$(document).ready(function(){
+function clickDiv(){
 
+  $(".row div").click(function(){
 
-$(".row div").click(function(){
-  var questoDiv = $(this);
-  var questoP = $(this).children("p");
-  $.ajax({
-    url : "https://flynn.boolean.careers/exercises/api/random/int",
-    method : "GET",
-    success : function (numeroRitorno) {
-      var num = numeroRitorno.response;
-      if (num <= 5) {
-        $(questoP).html(num);
-        $(questoDiv).css("background", "yellow");
-      } else if (num > 5) {
-        $(questoP).html(num);
-        $(questoDiv).css("background", "green");
+    var questoDiv = $(this);
+
+    $(questoDiv).addClass("active");
+
+    var questoP = $(this).children("p");
+
+    $.ajax({
+      url : "https://flynn.boolean.careers/exercises/api/random/int",
+
+      method : "GET",
+
+      success : function (numeroRitorno) {
+        var num = numeroRitorno.response;
+        if (num <= 5) {
+          $(questoP).html(num);
+          $(questoDiv).css("background", "yellow");
+        } else if (num > 5) {
+          $(questoP).html(num);
+          $(questoDiv).css("background", "green");
+        }
+      },
+
+      error : function (errore) {
+        alert("E' avvenuto un errore. ");
       }
-    },
-    error : function (richiesta,stato,errori) {
-      alert("E' avvenuto un errore. "+ errore);
-    }
+    });
   });
 
-});
 
+};
+$(document).ready(function(){
 
-
-
-
+  if ($(".row div").hasClass(".active") == true) {
+    $(this).unbind("click");
+  } else {
+    clickDiv();
+  };
 
 
 });
